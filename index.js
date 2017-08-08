fcd7ab621e5f7adad71820319d7c89292a8ad842
Prem <bit.prem@gmail.com>
2017-08-09 00:01:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,20 @@
  * Copyright(c) 2015 http://api.ai/
  * Apache 2.0 Licensed
  */
+var apiai = require('apiai');
 
-'use strict';
+var app = apiai("58b7a3867d0c4dbb93e729fb28089522");
 
-module.exports = require("./module/apiai");
+var request = app.textRequest('hi', {
+    sessionId: 'a4503331-f2a1-4056-a19e-9a7a6449b776'
+});
+
+request.on('response', function(response) {
+    console.log(response);
+});
+
+request.on('error', function(error) {
+    console.log(error);
+});
+
+request.end();
